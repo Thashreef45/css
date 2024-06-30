6ba92ee4e337d0d4dd33a030d501a517389aae97
Thashreef45 <ahmedthashreef@gmail.com>
2024-07-01 01:44:09 +0530

    test done

---
+++ grid/test.ts
@@ -0,0 +1,9 @@
+function hey(str:string) : string;
+function hey(a:number) : string;
+function hey(b:boolean) : string;
+
+// function hey(d : any):number;
+
+function hey(param: string | number | boolean ) : string{
+    return "Thashreef"
+}
